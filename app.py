63f614f2b4b69cd5f0cef31e4e654fedd0e3dffa
dbm <marko.s@msn.com>
2023-12-27 17:22:56 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+# Create Flask Instance
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    first_name = "John"
+    return render_template("index.html", first_name=first_name)
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+# Create Custom Error Pages
+
+
+# Invaild URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# Invaild Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
